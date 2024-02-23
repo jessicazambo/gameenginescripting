a8ddcf1838dc581df986f454eef0bb9bb874bb4c
JESSICACOMPUTER\Jessi <jessica.zambo@gmail.com>
2024-02-22 21:41:44 -0600

    downloaded week 5 files

--- Assets/Week-4/Scripts/Enemy.cs
+++ Assets/Week-4/Scripts/Enemy.cs
@@ -6,10 +6,16 @@ namespace ClassDemo
 {
     public class Enemy : MonoBehaviour
     {
+
         public int health = 10;
 
         [SerializeField] private Player target;
 
+
+        /// <summary>
+        /// Damages the enemy until they are dead
+        /// </summary>
+        /// <param name="amt">How much damage to deal the enemy</param>
         public void Damage(int amt)
         {
             health -= amt;
--- Assets/Week-4/Scripts/Player.cs
+++ Assets/Week-4/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,14 @@ namespace ClassDemo
 {
     public class Player : MonoBehaviour
     {
-        public int health = 10;
+        [SerializeField]
+        [Tooltip("How much damage the player can take")]
+        [Range(0f,100f)]
+        private float health = 10;
+
+        [SerializeField]
+        [TextArea(1,5)]
+        private string name;
 
         public void Damage(int amt)
         {
@@ -15,24 +23,53 @@ namespace ClassDemo
 
         private Enemy FindNewTarget()
         {
-            GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("EnemyTag");
+            /*GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("EnemyTag");
             int randomIndex = Random.Range(0, enemyObjects.Length);
-            return enemyObjects[randomIndex].GetComponent<Enemy>();
+            return enemyObjects[randomIndex].GetComponent<Enemy>();*/
 
-            /*GameObject enemyObject = GameObject.Find("EnemyObject");
+            /*GameObject enemyObject = GameObject.Find("Boss");
             return enemyObject.GetComponent<Enemy>();*/
 
-            /*Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+            Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
             int randomIndex = Random.Range(0, enemies.Length);
-            return enemies[randomIndex];*/
+            return enemies[randomIndex];
         }
 
         [ContextMenu("Attack")]
         void Attack()
         {
             Enemy target = FindNewTarget();
-            target.Damage(10);
+
+            Vector3 origin = transform.position;
+
+            transform.DOMove(target.transform.position, 1f)
+                     .OnComplete(() =>
+                     {
+                         target.Damage(10);
+                         SoundEffect attack = SoundManager.GetSound(SoundManager.SoundType.Attack);
+                         attack.Play(1f);
+
+
+                         transform.DOMove(origin, 1f);
+                     });
+
+
+           
+
+
+            
+           
+            //attack.SetLoop(true);
+            
+            //mAudioSource.Play();
         }
 
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+            }
+        }
     }
 }
