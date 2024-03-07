5da254af0418cd5bb8962cc8fae88c8dfacb735f
JESSICACOMPUTER\Jessi <jessica.zambo@gmail.com>
2024-03-07 17:28:16 -0600

    finishcharactercontroller

--- Assets/Week-6/Scripts/PlayerController.cs
+++ Assets/Week-6/Scripts/PlayerController.cs
@@ -9,18 +9,56 @@ namespace Week6
     {
 
         [SerializeField] float speed;
+        [SerializeField] float jumpForce;
+        [SerializeField] float rotationVertical = 5.0f;
+        [SerializeField] float rotationHorizontal = 5.0f;
 
-        public InputAction move;
+        private float mouseDeltaX = 0f; 
+        private float mouseDeltaY = 0f; 
+        private float cameraRotX = 0f; 
+        private int rotDir = 0;
+
+        PlayerControllerMappings playerMappings;
+
+        InputAction move;
+        InputAction fire;
+        InputAction jump;
+        InputAction look;
+
+        Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            playerMappings = new PlayerControllerMappings();
+            move = playerMappings.Player.Move;
+
+            fire = playerMappings.Player.Fire;
+            fire.performed += Fire;
+
+            jump = playerMappings.Player.Jump;
+            jump.performed += Jump;
+
+            look = playerMappings.Player.Look;
+
+        }
 
 
         private void OnEnable()
         {
             move.Enable();
+            fire.Enable();
+            jump.Enable();
+            look.Enable();
         }
 
         private void OnDisable()
         {
             move.Disable();
+            fire.Disable();
+            jump.Disable();
+            look.Disable();
         }
 
         // Start is called before the first frame update
@@ -29,20 +67,63 @@ namespace Week6
 
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            Vector2 input = move.ReadValue<Vector2>();
-
-            float x = transform.position.x - (input.x * speed * Time.deltaTime);
-
-            float y = transform.position.y;
-
-            float z = transform.position.z - (input.y * speed * Time.deltaTime);
-
-            transform.position = new Vector3(x, y, z);
+            HandleHorizontalRotation();
+            HandleVerticalRotation();
         }
 
+        void HandleHorizontalRotation() 
+        { 
+            mouseDeltaX = look.ReadValue<Vector2>().x; 
+            if (mouseDeltaX != 0) 
 
+            { 
+                rotDir = mouseDeltaX > 0 ? 1 : -1; 
+                transform.eulerAngles += new Vector3(0, rotationHorizontal * Time.deltaTime * rotDir, 0); 
+            } 
+        }
+        
+        void HandleVerticalRotation()
+        {
+            mouseDeltaY = look.ReadValue<Vector2>().y; 
+            
+            if (mouseDeltaY != 0)
+            {
+                rotDir = mouseDeltaY > 0 ? -1 : 1; 
+
+                cameraRotX += rotationVertical * Time.deltaTime * rotDir; 
+                cameraRotX = Mathf.Clamp(cameraRotX, -45f, 45f); 
+
+                var targetRotation = Quaternion.Euler(Vector3.right * cameraRotX); 
+                
+                //Vector3 angle = new Vector3(rotation * Time.deltaTime * rotDir, 0, 0);
+                
+                Debug.Log(Camera.main.transform.localRotation.x);
+                Camera.main.transform.localRotation = targetRotation; 
+                
+                //Camera.main.transform.Rotate(angle, Space.Self);
+            } 
+        }
+
+        // Update is called once per frame
+        void FixedUpdate() 
+        { 
+            Vector2 input = move.ReadValue<Vector2>(); 
+            Vector3 direction = (input.x * transform.right) + (transform.forward * input.y); 
+            
+            transform.position = transform.position + (direction * speed * Time.deltaTime); 
+        }
+
+        void Fire(InputAction.CallbackContext context)
+        {
+            Debug.Log("Fire!");
+        }
+
+        void Jump(InputAction.CallbackContext context)
+        {
+            Debug.Log("Jump!");
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
